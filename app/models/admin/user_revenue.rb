f6d5037e58a981f9ba1e0d0d30a5c4bbe1d01f69
Hoang Quan <hoang.van.quan@framgia.com>
2014-04-24 14:35:12 +0700

    edit user_revenue model

--- app/models/admin/user_revenue.rb
+++ app/models/admin/user_revenue.rb
@@ -1,5 +1,5 @@
 class Admin::UserRevenue < ActiveRecord::Base
 
-  belongs_to :user, class_name: User, foreign_key: :user_id
-  belongs_to :revenue, class_name: Admin::Revenue, foreign_key: :revenue_id
+  belongs_to :user, class_name: Admin::User.name, foreign_key: :user_id
+  belongs_to :revenue, class_name: Admin::Revenue.name, foreign_key: :revenue_id
 end
--- app/models/user_revenue.rb
+++ app/models/user_revenue.rb
@@ -1,1 +1,5 @@
-user_revenue.rb
+# class UserRevenue < ActiveRecord::Base
+
+#   belongs_to :user, class_name: User, foreign_key: :user_id
+#   belongs_to :revenue, class_name: Admin::Revenue, foreign_key: :revenue_id
+# end
