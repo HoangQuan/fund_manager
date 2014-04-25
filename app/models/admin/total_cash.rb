7787c71892a253cb7b9a68fa322f60a04e7bd3a9
Hoang Quan <hoang.van.quan@framgia.com>
2014-04-25 11:17:45 +0700

    fix total_cash modle

--- app/models/admin/total_cash.rb
+++ app/models/admin/total_cash.rb
@@ -2,5 +2,5 @@ class Admin::TotalCash < ActiveRecord::Base
   validates :current_total_cash, presence: true
   validates :current_total_cash, numericality: true
   validates :memo, presence: true
-  belongs_to :user, class_name: User, foreign_key: :created_user_id
+  belongs_to :user, class_name: Admin::User.name, foreign_key: :created_user_id
 end
