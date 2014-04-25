9b3dc0b09ca020ecd0ab9c8bdd97c33c680764f1
Hoang Quan <hoang.van.quan@framgia.com>
2014-04-25 14:49:49 +0700

    refactor some views

--- app/controllers/revenues_controller.rb
+++ app/controllers/revenues_controller.rb
@@ -1,5 +1,5 @@
 class RevenuesController < ApplicationController
-  
+  before_filter :signed_in_user
   def index
     @revenues = Admin::Revenue.order(:updated_at).page(params[:page]).per(5)
   end
@@ -7,5 +7,21 @@ class RevenuesController < ApplicationController
 
   def show
     @revenue = Admin::Revenue.find(params[:id])
+  end
+
+private
+  def signed_in_user
+    unless signed_in?
+      redirect_to root_path, notice: "Please sign in."
+    end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path)  unless current_user?(@user)
+  end
+  def admin_user
+    if current_user
+      redirect_to(root_path)  unless current_user.admin?
+    end
   end
 end
--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,10 +1,26 @@
 class SpendingsController < ApplicationController
-  
+  before_filter :signed_in_user
   def index
     @spendings = Admin::Spending.order(:updated_at).page(params[:page]).per(5)
   end
   
   def show
     @spending = Admin::Spending.find(params[:id])
+  end
+
+  private
+  def signed_in_user
+    unless signed_in?
+      redirect_to root_path, notice: "Please sign in."
+    end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path)  unless current_user?(@user)
+  end
+  def admin_user
+    if current_user
+      redirect_to(root_path)  unless current_user.admin?
+    end
   end
 end
